40206e12e6f375cd5ec70d7721a19fe8a629fd7c
Luis Enrique Ramos Cruz <lerc1991@gmail.com>
2023-03-06 14:43:45 -0600

    cambio diseño login

--- app/Views/login.php
+++ app/Views/login.php
@@ -11,7 +11,55 @@
     <title>Login</title>
   </head>
   <body>
-    <div class="container">
+
+  <section class="vh-100" style="background-color: #FF5733;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+            <img src="<?= base_url('assets/img/almacen2.jpg');?>" height="100%"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form action="<?php echo base_url('/login') ?>" method="post">
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                  <img src="<?= base_url('assets/img/logo.png');?>" alt="..." height="36">
+                    <span class="h3 fw-bold mb-0">Distribuidora el Tio</span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Ingresar al inventario</h5>
+
+                  <div class="form-outline mb-4">
+                    <input type="text" id="form2Example17" class="form-control form-control-lg" name="usuario" />
+                    <label class="form-label" for="form2Example17">Usuario</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" name="password" />
+                    <label class="form-label" for="form2Example27">Contraseña</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <input class="btn btn-dark btn-lg btn-block" type="submit"></input>
+                  </div>
+
+                  
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    <!--div class="container">
       
       <div class="row">
         <div class="col-sm-3"></div>
@@ -28,7 +76,7 @@
         </div>          
         <div class="col-sm-3"></div>  
       </div>
-    </div>
+    </div-->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -36,3 +84,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
+
+
